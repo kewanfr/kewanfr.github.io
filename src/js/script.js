10e815cb8d2c3a203afaac5369dd9a11b63bbe0d
Kéwan B <mail@kewan.fr>
2025-01-12 18:06:46 +0100

    portfolio illustration

--- src/js/script.js
+++ src/js/script.js
@@ -27,75 +27,54 @@ function closeMenu() {
 
 var projects = [
   {
-  "id": 1,
-  "name": "portfolio",
-  "description": "Site Web Personnel",
-  "page": "\n                  Le site sur lequel vous vous trouvez actuellement est mon portfolio. Il me permet de présenter mes compétences, mes projets et mon parcours. <br><br>\n                  J'ai développé les pages en HTML, CSS (enfin plutôt Tailwind CSS) et JavaScript. Les données sont stockées dans une base de données MySQL et récupérés via PHP.",
-  "repo_url": "https://github.com/kewanfr/kewanfr.github.io",
-  "demo_url": "https://kewan.fr",
-  "banner": "./src/img/projets/portfolio.jpg",
-  "date": "Décembre 2024",
-  "tags": [
-  "PHP",
-  "HTML/CSS/JS",
-  "TailwindCSS",
-  "MySQL"
-  ],
-  "tags_pinned": [
-  "PHP",
-  "HTML/CSS/JS",
-  "MySQL"
-  ]
+    id: 1,
+    name: "portfolio",
+    description: "Site Web Personnel",
+    page: "\n                  Le site sur lequel vous vous trouvez actuellement est mon portfolio. Il me permet de présenter mes compétences, mes projets et mon parcours. <br><br>\n                  J'ai développé les pages en HTML, CSS (enfin plutôt Tailwind CSS) et JavaScript. Les données sont stockées dans une base de données MySQL et récupérés via PHP.",
+    repo_url: "https://github.com/kewanfr/kewanfr.github.io",
+    demo_url: "https://kewan.fr",
+    banner: "./src/img/projets/site.jpg",
+    date: "Décembre 2024",
+    tags: ["PHP", "HTML/CSS/JS", "TailwindCSS", "MySQL"],
+    tags_pinned: ["PHP", "HTML/CSS/JS", "MySQL"],
   },
   {
-  "id": 2,
-  "name": "grades-watcher",
-  "description": "Notifications de Notes",
-  "page": "Ce projet est un projet personnel qui me permet de surveiller mes notes sur le site de l'IUT de Nantes. Je l'ai écrit en Node.js avec Puppeteer (c'est à dire un module qui permet de contrôler un navigateur web). <br /> <br />\n\nToutes les 15 minutes, le programme se connecte au site de l'IUT, récupère mes notes, les compare avec les notes précédentes et envoie un message sur Discord si une nouvelle note est apparue.",
-  "repo_url": "https://github.com/kewanfr/grades-watcher",
-  "demo_url": null,
-  "banner": "./src/img/projets/grades.jpg",
-  "date": "Décembre 2024",
-  "tags": [
-  "Node.Js",
-  "Puppeteer"
-  ],
-  "tags_pinned": [
-  "Node.Js",
-  "Puppeteer"
-  ]
+    id: 2,
+    name: "grades-watcher",
+    description: "Notifications de Notes",
+    page: "Ce projet est un projet personnel qui me permet de surveiller mes notes sur le site de l'IUT de Nantes. Je l'ai écrit en Node.js avec Puppeteer (c'est à dire un module qui permet de contrôler un navigateur web). <br /> <br />\n\nToutes les 15 minutes, le programme se connecte au site de l'IUT, récupère mes notes, les compare avec les notes précédentes et envoie un message sur Discord si une nouvelle note est apparue.",
+    repo_url: "https://github.com/kewanfr/grades-watcher",
+    demo_url: null,
+    banner: "./src/img/projets/grades.jpg",
+    date: "Décembre 2024",
+    tags: ["Node.Js", "Puppeteer"],
+    tags_pinned: ["Node.Js", "Puppeteer"],
   },
   {
-  "id": 3,
-  "name": "+ 100 Exos d'algorithmique",
-  "description": "Exos en cours à L'IUT",
-  "page": "Dans le cadre des cours \"Initiation au développement\" de l'IUT, nous avons une banque de 160 exercices d'algorithmique en Go. On doit essayer d'en résoudre pour s'entrainer à l'algorithmique. <br /> <br/>\r\n\r\nSur les 160, j'en ai actuellement résolu + de 120. Avec environ 1-2h par semaine sur 3 mois. <br/>\r\n\r\nC'est un défi intéressant car même les plus complexes deviennent simples avec un entrainement important.",
-  "repo_url": "https://github.com/kewanfr/tp-introdev",
-  "demo_url": null,
-  "banner": "./src/img/projets/exos-introdev.jpg",
-  "date": "year(current_timestamp())",
-  "tags": [
-  "Golang",
-  "Algorithmique"
-  ],
-  "tags_pinned": [
-  "Golang",
-  "Algorithmique"
-  ]
+    id: 3,
+    name: "+ 100 Exos d'algorithmique",
+    description: "Exos en cours à L'IUT",
+    page: "Dans le cadre des cours \"Initiation au développement\" de l'IUT, nous avons une banque de 160 exercices d'algorithmique en Go. On doit essayer d'en résoudre pour s'entrainer à l'algorithmique. <br /> <br/>\r\n\r\nSur les 160, j'en ai actuellement résolu + de 120. Avec environ 1-2h par semaine sur 3 mois. <br/>\r\n\r\nC'est un défi intéressant car même les plus complexes deviennent simples avec un entrainement important.",
+    repo_url: "https://github.com/kewanfr/tp-introdev",
+    demo_url: null,
+    banner: "./src/img/projets/exos-introdev.jpg",
+    date: "year(current_timestamp())",
+    tags: ["Golang", "Algorithmique"],
+    tags_pinned: ["Golang", "Algorithmique"],
   },
   {
-  "id": 4,
-  "name": "qr.kewan.fr",
-  "description": "Simple générateur de QR Codes",
-  "page": "J'ai développé le site <a href=\"https://qr.kewan.fr\">https://qr.kewan.fr</a> me permettant de simplement réaliser un QR Code instantanément.\r\nIl répondait à une demande que j'avais régulièrement, il m'est souvent compliqué de trouver un site pour en faire un rapidement. <br /> <br />\r\n\r\nSon programme est très simple, fait en HTML/CSS/JS Pur, et utilisant la librairie qrcode.js",
-  "repo_url": "https://github.com/kewanfr/qr-kewan-fr",
-  "demo_url": "https://qr.kewan.fr",
-  "banner": "./src/img/projets/qr.jpg",
-  "date": "2024",
-  "tags": [],
-  "tags_pinned": []
-  }
-  ];
+    id: 4,
+    name: "qr.kewan.fr",
+    description: "Simple générateur de QR Codes",
+    page: "J'ai développé le site <a href=\"https://qr.kewan.fr\">https://qr.kewan.fr</a> me permettant de simplement réaliser un QR Code instantanément.\r\nIl répondait à une demande que j'avais régulièrement, il m'est souvent compliqué de trouver un site pour en faire un rapidement. <br /> <br />\r\n\r\nSon programme est très simple, fait en HTML/CSS/JS Pur, et utilisant la librairie qrcode.js",
+    repo_url: "https://github.com/kewanfr/qr-kewan-fr",
+    demo_url: "https://qr.kewan.fr",
+    banner: "./src/img/projets/qr.jpg",
+    date: "2024",
+    tags: [],
+    tags_pinned: [],
+  },
+];
 
 let visibleProjects = DEFAULT_VISIBLE_PROJECTS; // Nombre de projets visibles au début
 const projectsContainer = document.getElementById("projects-container");
@@ -111,7 +90,11 @@ function displayProjects() {
   projects.slice(0, visibleProjects).forEach((project) => {
     const card = `
       <a href="/projet/${project.id}" class="project-card">
-        ${project.banner ? `<img src="${project.banner}" alt="${project.name}" />` : "<img src='./src/img/projets/default.jpg' alt='default' />"}
+        ${
+          project.banner
+            ? `<img src="${project.banner}" alt="${project.name}" />`
+            : "<img src='./src/img/projets/default.jpg' alt='default' />"
+        }
         <img src="${project.banner}" alt="Image du projet ${project.name}" />
         <div class="content">
           <!-- Infos visibles -->
@@ -119,7 +102,9 @@ function displayProjects() {
             <h3>${project.name || ""}</h3>
             <p class="date">${project.date || ""}</p>
             <div class="tags">
-              ${(project.tags_pinned || project.tags).map((tech) => `<span>${tech}</span>`).join("")}
+              ${(project.tags_pinned || project.tags)
+                .map((tech) => `<span>${tech}</span>`)
+                .join("")}
             </div>
           </div>
           <!-- Description au survol -->
@@ -239,7 +224,7 @@ var skills = {
 fetchSkills();
 
 fetchSkills().then((data) => {
-  skills = data
+  skills = data;
   displaySkills();
 });
 displaySkills();
