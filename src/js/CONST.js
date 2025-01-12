a0462f2367e7ca5c37d1e8b6f77e0eb8902d7d5f
Kéwan B <mail@kewan.fr>
2025-01-12 18:07:45 +0100

    base url

--- src/js/CONST.js
+++ src/js/CONST.js
@@ -1,7 +1,7 @@
 const DEFAULT_VISIBLE_PROJECTS = 5;
 const NUMBER_OF_PROJECTS_TO_LOAD = 3;
 
-const BASE_URL = 'https://kewan.fr/portfolio';
+const BASE_URL = './portfolio';
 
 const PROJECTS_URL = `${BASE_URL}/projets.php`;
 const POSTS_URL = `${BASE_URL}/posts`;
